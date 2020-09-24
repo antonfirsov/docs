a16f27e113205fde5e8c6bdc372335468e50bb04
Immo Landwerth <immol@microsoft.com>
2020-09-24 11:01:27 -0700

    Support validation of forward links

--- src/blog-validate/Rules/VR14_LinkShouldResolve.cs
+++ src/blog-validate/Rules/VR14_LinkShouldResolve.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Net.Http;
 
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
@@ -12,24 +13,43 @@ namespace BlogValidator
         {
             var links = context.Document.Descendants<LinkInline>();
 
+            var client = new HttpClient();
+
             foreach (var link in links)
             {
                 var url = new Uri(link.Url, UriKind.RelativeOrAbsolute);
                 if (!url.IsAbsoluteUri)
                     continue;
 
-                var request = WebRequest.Create(url);
                 try
                 {
-                    using (request.GetResponse())
-                    {
-                    }
+                    using var response = client.GetAsync(url).Result;
+
+                    if (IsForwardLink(url) && !IsForwarded(response))
+                        throw new Exception("The URL wasn't forwarded");
+
+                    if (IsForwarded(response))
+                        continue;
+
+                    response.EnsureSuccessStatusCode();
                 }
                 catch (Exception ex)
                 {
                     context.Warning("VR14", link, $"URL '{url}' doesn't resolve: {ex.Message}");
                 }
             }
+        }      
+
+        private bool IsForwardLink(Uri url)
+        {
+            return string.Equals(url.Host, "aka.ms", StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(url.Host, "go.microsoft.com", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private bool IsForwarded(HttpResponseMessage response)
+        {
+            return response.StatusCode == HttpStatusCode.Moved ||
+                   response.StatusCode == HttpStatusCode.MovedPermanently;
         }
     }
 }
