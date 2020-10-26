51eb1e16390d83cda299c6e437dea6c2e33c9b36
Immo Landwerth <immol@microsoft.com>
2020-10-26 15:48:39 -0700

    Only validate files that are part of the PR

--- tools/blog-validate/Program.cs
+++ tools/blog-validate/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+using LibGit2Sharp;
+
+using Mono.Options;
+
 namespace BlogValidator
 {
     // Maira suggested these rules:
@@ -17,22 +21,113 @@ namespace BlogValidator
     {
         static int Main(string[] args)
         {
-            if (args.Length != 1)
+            var exeName = Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0]);
+            var help = false;
+            var baseReferenceText = "";
+            var referenceText = "";
+            var inputPath = "";
+
+            var options = new OptionSet
             {
-                Console.Error.WriteLine("error: must specify an input path");
+                $"usage: {exeName} <directory> [OPTIONS]+",
+                { "base-ref=", "The branch the changes are merged into", v => baseReferenceText = v },
+                { "ref=", "The ref of the PR that is being merged", v => referenceText = v },
+                { "h|?|help", null, v => help = true, true },
+                new ResponseFileSource()
+            };
+
+            try
+            {
+                var parameters = options.Parse(args).ToArray();
+
+                if (help)
+                {
+                    options.WriteOptionDescriptions(Console.Error);
+                    return 0;
+                }
+
+                if (parameters.Length == 1)
+                {
+                    inputPath = parameters[0];
+                }
+                else
+                {
+                    Console.Error.WriteLine("error: must specify an input path");
+                    return 1;
+                }
+
+                var unprocessed = parameters.Skip(1);
+
+                if (unprocessed.Any())
+                {
+                    foreach (var option in unprocessed)
+                        Console.Error.WriteLine($"error: unrecognized argument {option}");
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
                 return 1;
             }
 
-            var directory = Path.GetFullPath(args[0]);
+            var directory = Path.GetFullPath(inputPath);
             if (!Directory.Exists(directory))
             {
                 Console.Error.WriteLine($"error: directory '{directory}' does not exist");
                 return 1;
             }
 
+            var affectedFiles = (string[])null;
+
+            if (!string.IsNullOrEmpty(baseReferenceText) || string.IsNullOrEmpty(referenceText))
+            {
+                if (string.IsNullOrEmpty(baseReferenceText))
+                {
+                    Console.Error.WriteLine($"error: must specify --base-ref when specifying --ref");
+                    return 1;
+                }
+
+                if (string.IsNullOrEmpty(referenceText))
+                {
+                    Console.Error.WriteLine($"error: must specify --ref when specifying --base-ref");
+                    return 1;
+                }
+
+                var repositoryPath = Repository.Discover(directory);
+                if (repositoryPath == null)
+                {
+                    Console.Error.WriteLine($"error: '{directory}' is not inside a Git repository");
+                    return 1;
+                }
+
+                var repository = new Repository(repositoryPath);
+
+                repository.RevParse(referenceText, out var reference, out var gitReferenceObject);
+                repository.RevParse(baseReferenceText, out var baseReference, out var gitBaseReferenceObject);
+
+                var referenceCommit = gitReferenceObject as Commit;
+                var baseReferenceCommit = gitBaseReferenceObject as Commit;
+
+                if (referenceCommit == null)
+                {
+                    Console.Error.WriteLine($"error: reference '{referenceText}' isn't valid");
+                    return 1;
+                }
+
+                if (baseReferenceCommit == null)
+                {
+                    Console.Error.WriteLine($"error: reference '{baseReferenceText}' isn't valid");
+                    return 1;
+                }
+
+                var treeChanges = repository.Diff.Compare<TreeChanges>(baseReferenceCommit.Tree, referenceCommit.Tree);
+                affectedFiles = treeChanges.Select(c => Path.GetFullPath(Path.Combine(repository.Info.WorkingDirectory, c.Path))).ToArray();
+            }
+
             try
             {
-                return Run(directory) ? 0 : 1;
+                return Run(directory, affectedFiles) ? 0 : 1;
             }
             catch (Exception ex)
             {
@@ -41,9 +136,9 @@ namespace BlogValidator
             }
         }
 
-        private static bool Run(string directory)
+        private static bool Run(string directory, string[] affectedFiles)
         {
-            var files = FindMarkdownFiles(directory);
+            var files = FindMarkdownFiles(directory, affectedFiles);
 
             var diagnostics = Validate(files);
 
@@ -77,10 +172,13 @@ namespace BlogValidator
             return !hasErrors;
         }
 
-        private static IEnumerable<string> FindMarkdownFiles(string directory)
+        private static IEnumerable<string> FindMarkdownFiles(string directory, string[] affectedFiles)
         {
+            if (affectedFiles != null)
+                return affectedFiles.Where(p => string.Equals(Path.GetExtension(p), ".md", StringComparison.OrdinalIgnoreCase));
+
             return Directory.GetFiles(directory, "*.md", SearchOption.AllDirectories)
-                                 .Where(p => IsIncluded(directory, p));
+                            .Where(p => IsIncluded(directory, p));
         }
 
         private static bool IsIncluded(string repoPath, string path)
