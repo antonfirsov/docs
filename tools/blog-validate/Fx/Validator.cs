f584671b38ec4055c4beae344a86ffc9cdaaa431
Immo Landwerth <immol@microsoft.com>
2021-08-13 11:23:27 -0700

    Output metadata when validating

--- tools/blog-validate/Fx/Validator.cs
+++ tools/blog-validate/Fx/Validator.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+
 namespace Microsoft.DotNetBlog
 {
     internal sealed class Validator
@@ -27,7 +30,19 @@ namespace Microsoft.DotNetBlog
         {
             var markdown = File.ReadAllText(fileName);
             var document = BlogMarkdown.Parse(markdown);
+
             var context = new ValidationContext(rootDirectory, document, fileName, categories);
+            var links = document.Descendants<LinkInline>().Select(l => l.Url).ToHashSet();
+
+            Console.WriteLine($"Root       : {context.RootDirectory}");
+            Console.WriteLine($"Path       : {context.FileName}");
+            Console.WriteLine($"Title      : {context.FrontMatter?.PostTitle}");
+            Console.WriteLine($"Summary    : {context.FrontMatter?.Summary}");
+            Console.WriteLine($"Categories : {context.FrontMatter?.Categories}");
+            Console.WriteLine($"Image      : {context.FrontMatter?.FeaturedImage}");
+            Console.WriteLine($"Username   : {context.FrontMatter?.Username}");
+            Console.WriteLine($"Alias      : {context.FrontMatter?.MicrosoftAlias}");
+            Console.WriteLine($"#Links     : {links.Count:N0}");
 
             foreach (var rule in _rules)
                 rule.Validate(context);
