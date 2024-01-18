e3c9f3eee79c64cf4b37d70c110529312e6b4e3a
Jon Galloway <jongalloway@gmail.com>
2024-01-18 14:53:18 -0800

    Codeblock language validation (#1480)
    * Update VR06 to warn if language isn't supported
    
    * Change from

--- tools/blog-validate/Rules/VR06_FencedCodeBlocksMustHaveLanguage.cs
+++ tools/blog-validate/Rules/VR06_FencedCodeBlocksMustHaveLanguage.cs
@@ -1,15 +1,27 @@
-﻿using Markdig.Syntax;
+using Markdig.Syntax;
 
 namespace Microsoft.DotNetBlog;
 
 internal sealed class VR06_FencedCodeBlocksMustHaveLanguage : ValidationRule
 {
+    // Create a string with a pipe delimited list of languages
+    private const string _languages = "accesslog,actionscript,apache,armasm,avrasm,bash,c,coffeescript,cpp,csharp,css,diff,django,dockerfile,dsconfig,fsharp,graphql,haml,html,http,java,javascript,json,latex,less,makefile,markdown,nginx,objectivec,php,plaintext,powershell,protobuf,ruby,scala,scss,sql,swift,typescript,vbnet,vbscript,vbscript-html,vim,x86asm,xml,yaml";
+
     public override void Validate(ValidationContext context)
     {
-        var unfencedBlocks = context.Document.Descendants<FencedCodeBlock>()
-                                             .Where(b => string.IsNullOrEmpty(b.Info));
+        var languages = new HashSet<string>(_languages.Split(','));
+        var blocks = context.Document.Descendants<FencedCodeBlock>().ToList();
 
-        foreach (var block in unfencedBlocks)
-            context.Error("VR06", block, "Fenced code blocks should specify a language");
+        foreach (var block in blocks)
+        {
+            if (string.IsNullOrEmpty(block.Info))
+            {
+                context.Error("VR06", block, "Fenced code blocks should specify a language");
+            }
+            else if (!languages.Contains(block.Info))
+            {
+                context.Warning("VR06", block, $"Fenced code blocks should specify a language from the following list: {_languages}");
+            }
+        }
     }
 }
